9cbb4377f0902ed43b3c5d5268c7870f1804c084
Dieter Burgstaller <dburgstaller@gmx.de>
2025-05-08 04:57:32 +0200

    #2 Using the Same Domain -- S1 Setting Up Laraveland Nuxt -- Nuxt 3 Authentication with Laravel11 Sanctum

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  devServer:{
+    host: "nuxt_backend.test"
+  }
 })

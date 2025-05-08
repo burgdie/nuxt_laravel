49420ededa47c3ce14a920f5c4a722438bbe6728
Dieter Burgstaller <dburgstaller@gmx.de>
2025-05-08 05:57:30 +0200

    #4 Install Nuxt-Auth-Sanctum Package -- S1 Setting Up Laraveland Nuxt -- Nuxt 3 Authentication with Laravel11 Sanctum

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -2,7 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false},
+
   devServer:{
     host: "nuxt_backend.test"
-  }
+  },
+
+  modules: ["nuxt-auth-sanctum"],
+
+  sanctum: {
+    baseUrl: 'http://nuxt_backend.test:80', // Laravel API
+},
 })

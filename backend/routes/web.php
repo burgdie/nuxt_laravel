3e32354cf2d03f45c11ee79585b595fd85955ca0
Dieter Burgstaller <dburgstaller@gmx.de>
2025-05-08 07:18:14 +0200

    #5 Making our First Request -- S1 Setting Up Laraveland Nuxt -- Nuxt 3 Authentication with Laravel11 Sanctum

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('login', function(){
+    return response()->json([
+        'message' => 'Login Success'
+    ]);
+
+});
---
+++ client/pages/auth/login.vue
@@ -0,0 +1,30 @@
+<script setup>
+const{login:loginAction} = useSanctumAuth();
+
+const form = reactive({
+  email: '',
+  password: ''
+});
+
+const login = async() => {
+  await loginAction(form);
+
+
+
+}
+
+
+</script>
+<template>
+  <div>
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <input v-model="form.email" type="email" name="" id="" placeholder="Email">
+      <input  v-model="form.password" type="password" name="" id="" placeholder="Password">
+      <button type="submit">Login</button>
+
+    </form>
+  </div>
+
+
+</template>

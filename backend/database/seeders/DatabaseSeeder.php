8d1452ea339850cf7093dc0bd4e6d786a70fc6a5
Dieter Burgstaller <dburgstaller@gmx.de>
2025-05-08 21:57:59 +0200

    #7_&_8 Authenticate with Fortify & Fetching Authenticated User -- S1 Setting Up Laraveland Nuxt -- Nuxt 3 Authentication with Laravel11 Sanctum

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Admin',
-            'email' => 'admin@.admin.com',
+            'email' => 'admin@admin.com',
         ]);
     }
 }
